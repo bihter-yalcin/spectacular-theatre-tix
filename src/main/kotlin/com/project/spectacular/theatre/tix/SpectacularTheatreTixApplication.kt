cfe157af0a44ff5bbd7090a8bcb326b9270df306
Bihter Yalçın <76825689+bihter-yalcin@users.noreply.github.com>
2024-05-21 23:47:31 +0300

    first commit

---
+++ src/main/kotlin/com/project/spectacular/theatre/tix/SpectacularTheatreTixApplication.kt
@@ -0,0 +1,11 @@
+package com.project.spectacular.theatre.tix
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpectacularTheatreTixApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpectacularTheatreTixApplication>(*args)
+}
---
+++ src/test/kotlin/com/project/spectacular/theatre/tix/SpectacularTheatreTixApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.project.spectacular.theatre.tix
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpectacularTheatreTixApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
